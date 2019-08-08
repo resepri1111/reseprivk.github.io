48ee37fbc0d0354e60f69241e76b5a831cd3aaca
resepri1111 <53897396+resepri1111@users.noreply.github.com>
2019-08-08 11:56:08 +0300

    Add files via upload

---
+++ precache-manifest.3800c1d06d025ffbab2977d2b907c657.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "a47e9ee1574017b21409",
+    "url": "./static/js/main.264899e2.chunk.js"
+  },
+  {
+    "revision": "3d132df72bad58c41ba4",
+    "url": "./static/js/2.3347830b.chunk.js"
+  },
+  {
+    "revision": "a47e9ee1574017b21409",
+    "url": "./static/css/main.026ffc53.chunk.css"
+  },
+  {
+    "revision": "3d132df72bad58c41ba4",
+    "url": "./static/css/2.81890415.chunk.css"
+  },
+  {
+    "revision": "006a3b7d9c480e0c635e248eac51c90e",
+    "url": "./index.html"
+  }
+];
